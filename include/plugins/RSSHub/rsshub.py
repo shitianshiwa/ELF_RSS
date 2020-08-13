fd1f7efe253386938ee78a5a6caa3f2d77e81133
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2020-08-13 17:40:24 +0900

    Update rsshub.py

--- include/plugins/RSSHub/rsshub.py
+++ include/plugins/RSSHub/rsshub.py
@@ -91,13 +91,11 @@ async def getRSS(rss:RSS_class.rss)->list:# 链接，订阅名
                         str_link = re.sub('member_illust.php\?mode=medium&illust_id=', 'i/', item['link'])
                         msg = msg + '原链接：' + str_link + '\n'
                         # msg = msg + '原链接：' + item['link'] + '\n'
-
                         try:
                             loc_time = time.mktime(item['published_parsed'])
-                            msg = msg + '日期：' + time.strftime("%m{}%d{} %H:%M:%S",
-                                                              time.localtime(loc_time + 28800.0)).format('月', '日')
+                            msg = msg + '日期：' + time.strftime("%Y{}%m{}%d{} %H:%M:%S",time.localtime(loc_time + 28800.0)).format('年','月', '日')
                         except BaseException:
-                            msg = msg + '日期：' + time.strftime("%m{}%d{} %H:%M:%S", time.localtime()).format('月', '日')
+                            msg = msg + '日期：' + time.strftime("%Y{}%m{}%d{} %H:%M:%S", time.localtime()).format('年','月', '日')
                         # print(msg+'\n\n\n')
                         await sendMsg(rss, msg, bot)
                         # msg_list.append(msg)
@@ -364,3 +362,34 @@ def writeRss(new, name):
         os.makedirs(file_path)
     with codecs.open(file_path + name + ".json", "w", 'utf-8') as dump_f:
         dump_f.write(json.dumps(old, sort_keys=True, indent=4, ensure_ascii=False))
+
+
+
+        #y两位年，Y四位年 
+'''
+python中时间日期格式化符号：
+
+%y 两位数的年份表示（00-99）
+%Y 四位数的年份表示（000-9999）
+%m 月份（01-12）
+%d 月内中的一天（0-31）
+%H 24小时制小时数（0-23）
+%I 12小时制小时数（01-12）
+%M 分钟数（00=59）
+%S 秒（00-59）
+%a 本地简化星期名称
+%A 本地完整星期名称
+%b 本地简化的月份名称
+%B 本地完整的月份名称
+%c 本地相应的日期表示和时间表示
+%j 年内的一天（001-366）
+%p 本地A.M.或P.M.的等价符
+%U 一年中的星期数（00-53）星期天为星期的开始
+%w 星期（0-6），星期天为星期的开始
+%W 一年中的星期数（00-53）星期一为星期的开始
+%x 本地相应的日期表示
+%X 本地相应的时间表示
+%Z 当前时区的名称
+%% %号本身
+https://www.runoob.com/python/att-time-strftime.html
+'''
